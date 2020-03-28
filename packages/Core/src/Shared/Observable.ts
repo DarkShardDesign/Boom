44568600e2a70769a07db5c60c1ec923ca28ae1b
Sean Przybyla <sean@darksharddesign.co.uk>
2020-03-28 13:55:37 +0100

    DSD-0004 refactored Observable

--- packages/Core/src/Shared/Observable.ts
+++ packages/Core/src/Shared/Observable.ts
@@ -1,34 +1,61 @@
+import { TObservable, TListener, ObservableConstants } from '../types';
+
 /**
  * Simple Observable object
  * either returns the observable or defines it on a parent if provided
- * @param {object?} parent the object to create this observable on
- * @param {string} name the name of this observale property
- * @param {any} value the value of this observable property
- * @returns {Observable} the observable property created
+ * @param { object? } parent the object to create this observable on
+ * @param { string } name the name of this observale property
+ * @param { any } value the value of this observable property
+ * @returns { TObservable } the observable property created
  */
-const Observable = (parent, name, value = undefined) => {
-    const _parent = parent || {};
+const Observable = (parent = {}, name, value = undefined): TObservable => {
+    const _parent = parent;
+    this.b = value;
+    this._observableListeners = <Array<TListener>>[];
+    if (!this._observableListeners) this._observableListeners = [];
+
+    /**
+     * used to unsubscribe a listener from this observable
+     * @param { TListener } listener the listener to remove
+     */
+    this.__proto__.unsubscribe = (listener: TListener) => {
+      return new Promise((resolve, reject) => {
+        if ( this._observableListeners.indexOf(listener) !== -1) {
+          this._observableListeners.splice(this._observableListeners.indexOf(listener), 1);
+          resolve(ObservableConstants.ListenerRemoved); // resolve as removed
+        }
+        resolve(ObservableConstants.ListenerNotFound); // resolve as not removed
+      })
+    }
+    
+    /**
+     * used to register a new listener to this observable
+     * @param { TListener } listener the listener to register
+     */
+    this.__proto__.listen = (listener: TListener) => {
+      this._observableListeners.push(listener);
+      return this.__proto__.unsubscribe.bind(this, listener);
+    }
+
+
+    this.__proto__.destroy = () => {
+        this._observableListeners.forEach((listener: TListener) => {
+          this._unsubscribe(listener);
+        });
+    }
+
     Object.defineProperty(_parent, name, {
       set(val) {
+        const old = this.b;
         // assign the new value
         this.b = val;
   
-        if (!this.observeListeners) this.observeListeners = [];
-        // make sure we have the listen func defined
-        this.__proto__.listen = func => this.observeListeners.push(func);
-        // make sure we have the unsubscribe func defined
-        this.__proto__.unsubscribe = func => {
-          if ( this.observeListeners.indexOf(func) !== -1) {
-              this.observeListeners.splice(this.observeListeners.indexOf(func), 1);
-          }
-        }
-  
         // inform any listeners of the new value
-        this.observeListeners.forEach(item => item(this.b));
+        this._observableListeners.forEach((listener: TListener) => listener(this.b, old, this));
       },
   
       // return the value property
-      get()  { return this.b; }
+      get()  { return this.b; },
     })
 
     // if we have an initial value use it
