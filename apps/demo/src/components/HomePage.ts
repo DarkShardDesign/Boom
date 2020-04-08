26bf1f4cafceea9b3bfe1e170010669b24c4ea5d
Sean Przybyla <sean@darksharddesign.co.uk>
2020-04-08 13:37:58 +0200

    DSD-23 improved readability of homepage.ts

--- apps/demo/src/components/HomePage.ts
+++ apps/demo/src/components/HomePage.ts
@@ -7,21 +7,22 @@ export default class HomePage extends WebComponent {
         this.componentName = 'home-page';
         this.isolated = false;
         this.parentElement = document.getElementById('app');
-        this.template = createTemplate(`
-        <style>
-        section {
-            display: flex;
-            flex: 1 1 100%;
-            flex-direction: row;
-            color: white;
-            background-color: blue;
-            padding: 5px;
-        }
-    </style>
-    <section>
-        <slot name="head">default header</slot>
-        <slot name="body">default body</slot>
-        <slot name="tail">default footer</slot>
-    </section>`);
+        const css = `
+            <style>
+                section {
+                    display: flex;
+                    flex: 1 1 100%;
+                    flex-direction: row;
+                    color: white;
+                    background-color: blue;
+                    padding: 5px;
+                }
+            </style>`;
+        const html = `<section>
+            <slot name="head">default header</slot>
+            <slot name="body">default body</slot>
+            <slot name="tail">default footer</slot>
+        </section>`;
+        this.template = createTemplate(css, html);
     }
 }
